1b1fa295862cc2f9af88d36f54caefee3ea26ce0
FriedLongJohns <81837862+FriedLongJohns@users.noreply.github.com>
2023-03-06 21:23:31 -0800

    initial
    add commands
    make goalPos feed from goalEnum
    enums can change values but reset upon cycling to not get broken

--- src/main/java/org/carlmontrobotics/robotcode2023/subsystems/Arm.java
+++ src/main/java/org/carlmontrobotics/robotcode2023/subsystems/Arm.java
@@ -36,7 +36,7 @@ public class Arm extends SubsystemBase {
   private ArmFeedforward armFeed = new ArmFeedforward(kS, kG, kV, kA);
   private PIDController pid = new PIDController(kP, kI, kD);
   
-  private double goalPos;
+  public ArmPreset goalEnum;
   private double EncoderPos = encoder.getZeroOffset();
     
   public double hiClamp = -Math.PI*.5; //TODO GET NUMBERS
@@ -46,9 +46,21 @@ public class Arm extends SubsystemBase {
     INTAKE(0.31), MID(-1.74), HIGH(-1.83);
     
     public double value; //not static so SmartDashboard can touch [IMPORTANT TO KNOW!]
+    private double origin;
     ArmPreset(double value) {
       this.value = value;
+      this.origin = value;
     }
+    private void reset(){
+        this.value = this.origin;//for when we manually set the goalEnum pos, when we cycle this will fix itself
+    }
+    public ArmPreset next() { this.reset(); return this.values()[(this.ordinal()+1) % this.values().length]; }
+    public ArmPreset prev() { this.reset(); return this.values()[(this.ordinal()+2) % this.values().length]; }
+    //                           -1 but then +3 so that looping works correctly^
+      
+    //ordinal : after adding : after mod operator
+    //next() 0:1:1, 1:2:2, 2:3:0
+    //prev() 0:2:2, 1:3:0, 2:4:1 
   }
 
   public Arm() {
@@ -56,7 +68,7 @@ public class Arm extends SubsystemBase {
     encoder.getZeroOffset();
     pid.setTolerance(2.5,10);
     
-    SmartDashboard.putNumber("GoalPosition", goalPos);
+    SmartDashboard.putNumber("GoalPosition", goalEnum.value);
   }
 
   @Override
@@ -66,20 +78,20 @@ public class Arm extends SubsystemBase {
     pid.setD(kD);
      double currentPos = encoder.getZeroOffset();
     
-    goalPos = MathUtil.clamp(goalPos, loClamp, hiClamp);
+    double goalPos = MathUtil.clamp(goalEnum.value, loClamp, hiClamp);
       
-      motor.setVoltage(armFeed.calculate(currentPos, 0, 0)
-         + pid.calculate(currentPos, goalPos));
-         
-         goalPos = SmartDashboard.getNumber("GoalPosition", goalPos);
-         SmartDashboard.getNumber("EncoderPos", currentPos);
+    motor.setVoltage(armFeed.calculate(currentPos, 0, 0)
+      + pid.calculate(currentPos, goalPos));
+
+    goalPos = SmartDashboard.getNumber("GoalPosition", goalPos);
+    SmartDashboard.getNumber("EncoderPos", currentPos);
   }
   @Override
   public void initSendable(SendableBuilder builder) {
     super.initSendable(builder);
     builder.addDoubleProperty("FF: Velocity", () -> FFvelocity, x -> this.FFvelocity = x);
     builder.addDoubleProperty("FF: Accel",    () -> FFaccel,    x -> this.FFaccel = x);
-    builder.addDoubleProperty("goalPos",      () -> goalPos,    x -> this.goalPos = x);
+    builder.addDoubleProperty("goalPos",      () -> goalEnum.value,    x -> this.goalEnum.value = x);
     builder.addDoubleProperty("EncoderPos",   () -> EncoderPos, x -> this.EncoderPos = x);
     builder.addDoubleProperty("kP",           () -> kP,         x -> this.kP = x);
     builder.addDoubleProperty("kI",           () -> kI,         x -> this.kI = x);
@@ -92,5 +104,34 @@ public class Arm extends SubsystemBase {
   
   public void setPreset(ArmPreset preset) {
    // SmartDashboard.putNumber("GoalPosition", preset.value);
+  }
+    
+  //Snaps raw encoder pos to one of our cycle positions
+  public ArmPreset snappedArmPos() {
+    double encoderPos = encoder.getZeroOffset();
+
+    for(ArmPreset check : ArmPreset.values()) {
+      double lowdist = (check.value - check.prev().value) / 2;
+      double hidist = (check.next().value - check.value) / 2; // get the halfway points between each position and it's neighbors
+      if (check.value - lowdist < encoderPos && encoderPos < check.value + hidist) {
+        //seperate high and low instead of ABS because maybe difference isn't constant between each position of arm
+        //and yes it still works for lowest and highest value
+        return check;
+      }
+    }
+    //help something went REALLY wrong
+    return null;
+  }
+
+  public ArmPreset closeSnappedArmPos() {//more precise snapping
+    double encoderPos = encoder.getZeroOffset();
+
+    for(ArmPreset check : ArmPreset.values()) {
+        if (Math.abs(check.value - encoderPos) > encoderErrorTolerance) {//maybe will break if cone/cube values are close, but if they are close then lower error or only use one enum
+        return check;
+        }
+    }
+    //nothing close enough
+    return null;
   }
 }
